c8f55c3d43aaa6de1e5b3f4dbba7acd5fad6fe7f
teramako <teramako@gmail.com>
2025-01-25 00:05:11 +0900

    :sparkles: Implement `ICacheableResource` for SystemJob

--- src/Jagabata/Resources/SystemJob.cs
+++ src/Jagabata/Resources/SystemJob.cs
@@ -1,4 +1,5 @@
 using System.Collections.Specialized;
+using System.Text;
 
 namespace Jagabata.Resources
 {
@@ -29,7 +30,7 @@ namespace Jagabata.Resources
                            string extraVars, string resultStdout)
         : UnifiedJob(id, type, url, created, modified, name, launchType, status, executionEnvironment, failed,
                      started, finished, canceledOn, elapsed, jobExplanation, launchedBy, workUnitId),
-          ISystemJob, IResource
+          ISystemJob, IResource, ICacheableResource
     {
         public new const string PATH = "/api/v2/system_jobs/";
         /// <summary>
@@ -98,6 +99,22 @@ namespace Jagabata.Resources
         public Dictionary<string, object?> GetExtraVars()
         {
             return Yaml.DeserializeToDict(ExtraVars);
+        }
+
+        public string GetDescription()
+        {
+            var sb = new StringBuilder(Name);
+            if (!string.IsNullOrEmpty(Description))
+            {
+                sb.Append($" ({Description})");
+            }
+            sb.Append($" Status={Status}");
+            if (Finished is not null)
+            {
+                sb.Append($" Finished={Finished}");
+            }
+            sb.Append($" Elapsed={Elapsed}");
+            return sb.ToString();
         }
     }
 }
