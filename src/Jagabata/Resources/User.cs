7e395e5c216e18fc085b83e15e54d80d76663ca8
teramako <teramako@gmail.com>
2025-04-01 11:44:23 +0000

    :+1: Implement `GetTeams()` for User class

--- src/Jagabata/Resources/User.cs
+++ src/Jagabata/Resources/User.cs
@@ -191,6 +191,32 @@ namespace Jagabata.Resources
             return [.. GetResultsByRelatedKey<ActivityStream>("activity_stream", query)];
         }
 
+        /// <summary>
+        /// Get the teams related to this user
+        /// <para>
+        /// Implement API: <c>/api/v2/users/{id}/teams/</c>
+        /// </para>
+        /// </summary>
+        /// <param name="searchWords"></param>
+        /// <param name="orderBy">Sort keys (<c>','</c> separated values)</param>
+        /// <param name="pageSize">Max number to retrieve</param>.
+        public Team[] GetTeams(string? searchWords = null, string orderBy = "name", ushort pageSize = 20)
+        {
+            return [.. GetResultsByRelatedKey<Team>("teams", searchWords, orderBy, pageSize)];
+        }
+
+        /// <summary>
+        /// Get the teams related to this user
+        /// <para>
+        /// Implement API: <c>/api/v2/users/{id}/teams/</c>
+        /// </para>
+        /// </summary>
+        /// <param name="query">Full customized queries (filtering, sorting and paging)</param>.
+        public Team[] GetTeams(HttpQuery query)
+        {
+            return [.. GetResultsByRelatedKey<Team>("teams", query)];
+        }
+
         protected override CacheItem GetCacheItem()
         {
             var item = new CacheItem(Type, Id, Username, string.Empty);
