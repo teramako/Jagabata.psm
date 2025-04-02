dad1920634c24d65d3597bab32b5af7f281c5e24
teramako <teramako@gmail.com>
2025-04-02 12:54:04 +0000

    :+1: Implement `GetHosts()` for InventorySource class

--- src/Jagabata/Resources/InventorySource.cs
+++ src/Jagabata/Resources/InventorySource.cs
@@ -365,6 +365,32 @@ namespace Jagabata.Resources
                 : null;
         }
 
+        /// <summary>
+        /// Get the hosts related to this inventory source
+        /// <para>
+        /// Implement API: <c>/api/v2/inventory_sources/{id}/hosts/</c>
+        /// </para>
+        /// </summary>
+        /// <param name="searchWords"></param>
+        /// <param name="orderBy">Sort keys (<c>','</c> separated values)</param>
+        /// <param name="pageSize">Max number to retrieve</param>.
+        public Host[] GetHosts(string? searchWords = null, string orderBy = "name", ushort pageSize = 20)
+        {
+            return [.. GetResultsByRelatedKey<Host>("hosts", searchWords, orderBy, pageSize)];
+        }
+
+        /// <summary>
+        /// Get the hosts related to this inventory source
+        /// <para>
+        /// Implement API: <c>/api/v2/inventory_sources/{id}/hosts/</c>
+        /// </para>
+        /// </summary>
+        /// <param name="query">Full customized queries (filtering, sorting and paging)</param>.
+        public Host[] GetHosts(HttpQuery query)
+        {
+            return [.. GetResultsByRelatedKey<Host>("hosts", query)];
+        }
+
         protected override CacheItem GetCacheItem()
         {
             var item = new CacheItem(Type, Id, Name, Description)
