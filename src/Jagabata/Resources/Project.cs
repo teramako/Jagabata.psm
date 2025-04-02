ca31d47d9fc9585e293bdc2481430beedfca1b76
teramako <teramako@gmail.com>
2025-04-02 11:58:44 +0000

    :+1: Implement `GetObjectRoles()` for Project class

--- src/Jagabata/Resources/Project.cs
+++ src/Jagabata/Resources/Project.cs
@@ -437,6 +437,20 @@ namespace Jagabata.Resources
             return [.. GetResultsByRelatedKey<User>("access_list", query)];
         }
 
+        /// <summary>
+        /// Get the object roles related to this project
+        /// </summary>
+        /// <remarks>
+        /// This is almost same as:
+        /// <code>thisObject.SummaryFields["ObjectRoles"]</code>
+        /// </remarks>
+        public ObjectRoleSummary[] GetObjectRoles()
+        {
+            return SummaryFields.TryGetValue<Dictionary<string, ObjectRoleSummary>>("ObjectRoles", out var dict)
+                ? [.. dict.Values]
+                : [];
+        }
+
         protected override CacheItem GetCacheItem()
         {
             return new CacheItem(Type, Id, Name, Description)
