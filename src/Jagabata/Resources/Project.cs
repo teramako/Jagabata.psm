007d69a18e51703aeb1206efdd04b4f4a6140fe7
teramako <teramako@gmail.com>
2025-04-02 11:54:22 +0000

    :+1: Implement `GetTeams()` for Project class

--- src/Jagabata/Resources/Project.cs
+++ src/Jagabata/Resources/Project.cs
@@ -292,6 +292,32 @@ namespace Jagabata.Resources
             return [.. GetResultsByRelatedKey<ActivityStream>("activity_stream", query)];
         }
 
+        /// <summary>
+        /// Get the teams related to this project
+        /// <para>
+        /// Implement API: <c>/api/v2/projects/{id}/teams/</c>
+        /// </para>
+        /// </summary>
+        /// <param name="searchWords"></param>
+        /// <param name="orderBy">Sort keys (<c>','</c> separated values)</param>
+        /// <param name="pageSize">Max number to retrieve</param>.
+        public Team[] GetTeams(string? searchWords = null, string orderBy = "name", ushort pageSize = 20)
+        {
+            return [.. GetResultsByRelatedKey<Team>("teams", searchWords, orderBy, pageSize)];
+        }
+
+        /// <summary>
+        /// Get the teams related to this project
+        /// <para>
+        /// Implement API: <c>/api/v2/projects/{id}/teams/</c>
+        /// </para>
+        /// </summary>
+        /// <param name="query">Full customized queries (filtering, sorting and paging)</param>.
+        public Team[] GetTeams(HttpQuery query)
+        {
+            return [.. GetResultsByRelatedKey<Team>("teams", query)];
+        }
+
         protected override CacheItem GetCacheItem()
         {
             return new CacheItem(Type, Id, Name, Description)
