962329ffe12aeb482a2fc3f7123b9184164cf229
teramako <teramako@gmail.com>
2025-04-02 13:24:07 +0000

    :+1: Implement `GetCredentials()` for InventorySource class

--- src/Jagabata/Resources/InventorySource.cs
+++ src/Jagabata/Resources/InventorySource.cs
@@ -530,6 +530,32 @@ namespace Jagabata.Resources
                 : null;
         }
 
+        /// <summary>
+        /// Get the credentials related to this inventory source
+        /// <para>
+        /// Implement API: <c>/api/v2/inventory_sources/{id}/credentials/</c>
+        /// </para>
+        /// </summary>
+        /// <param name="searchWords"></param>
+        /// <param name="orderBy">Sort keys (<c>','</c> separated values)</param>
+        /// <param name="pageSize">Max number to retrieve</param>.
+        public Credential[] GetCredentials(string? searchWords = null, string orderBy = "name", ushort pageSize = 20)
+        {
+            return [.. GetResultsByRelatedKey<Credential>("credentials", searchWords, orderBy, pageSize)];
+        }
+
+        /// <summary>
+        /// Get the credentials related to this inventory source
+        /// <para>
+        /// Implement API: <c>/api/v2/inventory_sources/{id}/credentials/</c>
+        /// </para>
+        /// </summary>
+        /// <param name="query">Full customized queries (filtering, sorting and paging)</param>.
+        public Credential[] GetCredentials(HttpQuery query)
+        {
+            return [.. GetResultsByRelatedKey<Credential>("credentials", query)];
+        }
+
         protected override CacheItem GetCacheItem()
         {
             var item = new CacheItem(Type, Id, Name, Description)
