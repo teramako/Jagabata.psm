f03d1d5c002534c18b1ada66ebc9c8b6f3858459
teramako <teramako@gmail.com>
2025-04-05 06:40:59 +0000

    :+1: Implement `GetAccessList()` for WorkflowJobTemplate class

--- src/Jagabata/Resources/WorkflowJobTemplate.cs
+++ src/Jagabata/Resources/WorkflowJobTemplate.cs
@@ -359,6 +359,32 @@ namespace Jagabata.Resources
             return [.. GetResultsByRelatedKey<NotificationTemplate>("notification_templates_approvals", query)];
         }
 
+        /// <summary>
+        /// Get the access list related to this workflow job template
+        /// <para>
+        /// Implement API: <c>/api/v2/workflow_job_templates/{id}/access_list/</c>
+        /// </para>
+        /// </summary>
+        /// <param name="searchWords"></param>
+        /// <param name="orderBy">Sort keys (<c>','</c> separated values)</param>
+        /// <param name="pageSize">Max number to retrieve</param>.
+        public User[] GetAccessList(string? searchWords = null, string orderBy = "username", ushort pageSize = 20)
+        {
+            return [.. GetResultsByRelatedKey<User>("access_list", searchWords, orderBy, pageSize)];
+        }
+
+        /// <summary>
+        /// Get the access list related to this workflow job template
+        /// <para>
+        /// Implement API: <c>/api/v2/workflow_job_templates/{id}/access_list/</c>
+        /// </para>
+        /// </summary>
+        /// <param name="query">Full customized queries (filtering, sorting and paging)</param>.
+        public User[] GetAccessList(HttpQuery query)
+        {
+            return [.. GetResultsByRelatedKey<User>("access_list", query)];
+        }
+
         protected override CacheItem GetCacheItem()
         {
             return new CacheItem(Type, Id, Name, Description);
