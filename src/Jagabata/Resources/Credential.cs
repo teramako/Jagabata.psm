6900b2b02462a3a35451f4b5493889383096f59a
teramako <teramako@gmail.com>
2025-04-10 12:42:01 +0000

    :+1: Implement `GetAccessList()` for Credential class

--- src/Jagabata/Resources/Credential.cs
+++ src/Jagabata/Resources/Credential.cs
@@ -331,6 +331,32 @@ namespace Jagabata.Resources
             return [.. GetResultsByRelatedKey<ActivityStream>("activity_stream", query)];
         }
 
+        /// <summary>
+        /// Get the access list related to this credential
+        /// <para>
+        /// Implement API: <c>/api/v2/credentials/{id}/access_list/</c>
+        /// </para>
+        /// </summary>
+        /// <param name="searchWords"></param>
+        /// <param name="orderBy">Sort keys (<c>','</c> separated values)</param>
+        /// <param name="pageSize">Max number to retrieve</param>.
+        public User[] GetAccessList(string? searchWords = null, string orderBy = "username", ushort pageSize = 20)
+        {
+            return [.. GetResultsByRelatedKey<User>("access_list", searchWords, orderBy, pageSize)];
+        }
+
+        /// <summary>
+        /// Get the access list related to this credential
+        /// <para>
+        /// Implement API: <c>/api/v2/credentials/{id}/access_list/</c>
+        /// </para>
+        /// </summary>
+        /// <param name="query">Full customized queries (filtering, sorting and paging)</param>.
+        public User[] GetAccessList(HttpQuery query)
+        {
+            return [.. GetResultsByRelatedKey<User>("access_list", query)];
+        }
+
         public override string ToString()
         {
             return string.IsNullOrEmpty(Kind) ? $"{Type}:{Id}:{Name}" : $"{Type}:{Id}:{Kind}:{Name}";
