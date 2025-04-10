bbee38a909bc6a356d433536d64423742dfd4a2a
teramako <teramako@gmail.com>
2025-04-10 11:51:29 +0000

    :+1: Implement `GetInstanceGroups()` for WorkflowJobNode class

--- src/Jagabata/Resources/WorkflowJobNode.cs
+++ src/Jagabata/Resources/WorkflowJobNode.cs
@@ -275,6 +275,32 @@ namespace Jagabata.Resources
             return [.. GetResultsByRelatedKey<Credential>("credentials", query)];
         }
 
+        /// <summary>
+        /// Get the instance groups related to this workflow job node
+        /// <para>
+        /// Implement API: <c>/api/v2/workflow_job_nodes/{id}/instance_groups/</c>
+        /// </para>
+        /// </summary>
+        /// <param name="searchWords"></param>
+        /// <param name="orderBy">Sort keys (<c>','</c> separated values)</param>
+        /// <param name="pageSize">Max number to retrieve</param>.
+        public InstanceGroup[] GetInstanceGroups(string? searchWords = null, string orderBy = "name", ushort pageSize = 20)
+        {
+            return [.. GetResultsByRelatedKey<InstanceGroup>("instance_groups", searchWords, orderBy, pageSize)];
+        }
+
+        /// <summary>
+        /// Get the instance groups related to this workflow job node
+        /// <para>
+        /// Implement API: <c>/api/v2/workflow_job_nodes/{id}/instance_groups/</c>
+        /// </para>
+        /// </summary>
+        /// <param name="query">Full customized queries (filtering, sorting and paging)</param>.
+        public InstanceGroup[] GetInstanceGroups(HttpQuery query)
+        {
+            return [.. GetResultsByRelatedKey<InstanceGroup>("instance_groups", query)];
+        }
+
         protected override CacheItem GetCacheItem()
         {
             var item = new CacheItem(Type, Id, string.Empty, string.Empty);
